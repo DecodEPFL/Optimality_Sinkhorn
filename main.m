943f377a6cd0d9c0818ce3e59807753730d751d3
Riccardo Cescon <cesconriccardo1@gmail.com>
2025-08-21 17:35:04 +0200

    add tests

--- main.m
+++ main.m
@@ -1,15 +1,15 @@
 close all; clearvars; clc;
-rng(666);
+rng(1234);
 
 %% Parameters
-rho = 5;
+rho = 10;
 eps = 1e-1;
 T = 10;
 n = 2; m = 2; p = 2;
-A = [1 1; 0 1];
+A = [1.1 0.1 ; 0 .95];
 B = eye(m);
 C = eye(p);
-Q = eye(n);
+Q = 10*eye(n);
 R = eye(m);
 S = eye(n);
 
@@ -19,7 +19,6 @@ Vh = cell(T,1);
 for t = 1:T
     Wh{t} = .1 * random_psd_matrix(n, 1, 2);
     Vh{t} = .1 * random_psd_matrix(p, 1, 2);
-    % Vh{t} = 1e-10*eye(p);
 end
 X0h = .1 * random_psd_matrix(n, 1, 2);
 
@@ -85,12 +84,10 @@ D = CT * G;
 sls.A = kron(eye(T+1), A);
 sls.B = kron(eye(T+1), B);
 sls.B = sls.B(:, 1:T*m);
-% sls.C = kron(eye(T), C);
 sls.C = CT;
 sls.I = eye(n*(T+1));
 sls.Z = [zeros(n, n*(T)) zeros(n, n); eye(n*(T)) zeros(n*T, n)];
 
-% [~, ~, test] = debugSLS(sls, m, n, T, blkdiag(QT, RT), blkdiag(Wh{:}));
 [Phi_xx_nom, Phi_xy_nom, Phi_ux_nom, Phi_uy_nom, cost_nom] = causal_unconstrained_h2(m, p, T, blkdiag(QT, RT), sls, blkdiag(X0h, Wh{:}, Vh{:}));
 % [Phi_xxW, Phi_xyW, Phi_uxW, Phi_uyW, costW] = causal_unconstrained_h2(m, p, T, blkdiag(QT, RT), sls, blkdiag(Ww, Vw));
 [Phi_xxS, Phi_xyS, Phi_uxS, Phi_uyS, costS] = causal_unconstrained_h2(m, p, T, blkdiag(QT, RT), sls, blkdiag(Ws, Vs));
@@ -128,11 +125,11 @@ h2.FaceAlpha = 0.2;
 yl = ylim; % current y-axis limits
 plot([mu_nom mu_nom], yl, 'b--','LineWidth',2);
 plot([mu_rob mu_rob], yl, 'r--','LineWidth',2);
-set(gca,'XScale','log');
+% set(gca,'XScale','log');
 
 %% Test policies off-sample
 for i=1:mc_tests
-    % sample from the nominal Gaussian
+    % sample from the worst-case Gaussian
     delta = mvnrnd(zeros(T*(n+p) + n, 1), blkdiag(Ws, Vs))';   % column vector sample
     tmp = delta' * Phi_nom' * blkdiag(QT, RT) * Phi_nom * delta;
     tmp1 = delta' * Phi_S' * blkdiag(QT, RT) * Phi_S * delta;
@@ -158,6 +155,46 @@ yl = ylim; % current y-axis limits
 plot([mu_nom mu_nom], yl, 'b--','LineWidth',2);
 plot([mu_rob mu_rob], yl, 'r--','LineWidth',2);
 set(gca,'XScale','log');
+
+%% Test policies on unseen noise realizations
+for i=1:mc_tests
+
+    W_new = cell(T,1);
+    V_new = cell(T,1);
+
+    % sample new covariances
+    W_1 = random_psd_matrix(n, 0, 1);
+    for t=1:T
+        W_new{t} = random_psd_matrix(n, 0, 1);
+        V_new{t} = random_psd_matrix(p, 0, 1);
+    end
+
+    delta = mvnrnd(zeros(T*(n+p) + n, 1), blkdiag(W_1, W_new{:}, V_new{:}))';   % column vector sample
+    tmp = delta' * Phi_nom' * blkdiag(QT, RT) * Phi_nom * delta;
+    tmp1 = delta' * Phi_S' * blkdiag(QT, RT) * Phi_S * delta;
+    nom.cost_unseen(i) = tmp;
+    rob.cost_unseen(i) = tmp1;
+end
+
+mu_nom = mean(nom.cost_unseen);
+mu_rob = mean(rob.cost_unseen);
+
+figure;
+h1 = histogram(nom.cost_unseen, 100);
+hold on
+h2 = histogram(rob.cost_unseen, 100);
+% Imposta colore e trasparenza
+h1.FaceColor = 'b';    
+h1.FaceAlpha = 0.8;    
+h2.FaceColor = 'r';    
+h2.FaceAlpha = 0.2;    
+
+% Add vertical lines at the means
+yl = ylim; % current y-axis limits
+plot([mu_nom mu_nom], yl, 'b--','LineWidth',2);
+plot([mu_rob mu_rob], yl, 'r--','LineWidth',2);
+set(gca,'XScale','log');
+
 %% Auxiliary functions
 function A = random_psd_matrix(n, lambda_min, lambda_max)
 % Generate a symmetric positive definite matrix with eigenvalues in [lambda_min, lambda_max]
@@ -202,43 +239,4 @@ function [Phi_xx, Phi_xy, Phi_ux, Phi_uy, cost] = causal_unconstrained_h2(m, p, 
     Phi_ux = value(Phi_ux);
     Phi_uy = value(Phi_uy);
     cost = value(objective);
-end
-
-function [Phi_x, Phi_u, objective] = debugSLS(sls, m, n, T, C, Sigma)
-%CAUSAL_UNCONSTRAINED computes an unconstrained causal linear control
-%policy that is optimal either in the H2 or in the Hinf sense
-    
-    % Define the decision variables of the optimization problem
-    Phi_u = sdpvar(m*T, n*T, 'full');
-    Phi_x = (sls.I - sls.Z*sls.A) \ (sls.Z*sls.B*Phi_u + sls.I); % Phi_x is given as function of Phi_u
-    
-    % Define the objective function
-
-    objective = norm(sqrtm(C)*[Phi_x; Phi_u]*sqrtm(Sigma), 'fro');
-    constraints = [];
-    % % Impose the achievability constraints
-    % constraints = [constraints, (sls.I - sls.Z*sls.A)*Phi_x - sls.Z*sls.B*Phi_u == sls.I];
-
-    % Impose the causal sparsities on the closed loop responses
-    for i = 0:T-2
-        for j = i+1:T-1 % Set j from i+2 for non-strictly causal controller (first element in w is x0)
-            % constraints = [constraints, Phi_x((1+i*sys.d):((i+1)*sys.d), (1+j*sys.d):((j+1)*sys.d)) == zeros(sys.d, sys.d)];
-            constraints = [constraints, Phi_u((1+i*m):((i+1)*m), (1+j*n):((j+1)*n)) == zeros(m, n)];
-        end
-    end
-    
-    % Solve the optimization problem
-    options = sdpsettings('verbose', 0, 'solver', 'mosek');
-    sol = optimize(constraints, objective, options);
-    if ~(sol.problem == 0)
-        error('Something went wrong...');
-    end
-    
-    % Extract the closed-loop responses corresponding to a unconstrained causal 
-    % linear controller that is optimal either in the H2 or in the Hinf sense
-    Phi_x = value(Phi_x); 
-    Phi_u = value(Phi_u);
-    
-    objective = value(objective)^2; % Extract the H2- or Hinf-optimal cost incurred by an unconstrained causal linear controller
-
 end
